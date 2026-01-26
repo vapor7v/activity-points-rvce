d837307a67436ea054e54911095cb90493b5ba6c
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2026-01-26 21:56:29 +0530

    Feature: Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/global/theme-provider"
 import QueryProvider from "@/components/global/query-provider";
 import { Toaster } from '@/components/ui/sonner';
+import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,6 +37,16 @@ export default function RootLayout({
             >
               {children}
             </ThemeProvider>
+             <Analytics />
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+          </Script>
           </QueryProvider>
         <Toaster position="top-center" />
       </body>
